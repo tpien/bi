8abc91d8fcbb3ad44c580267fba729a6eb0d56da
tstepien <tomasz.stepien@comarch.pl>
2024-01-06 08:41:37 +0100

    Spring init

---
+++ src/main/java/com/resteurant/bi/poc/skytabbi/SkytabbiApplication.java
@@ -0,0 +1,12 @@
+package com.resteurant.bi.poc.skytabbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkytabbiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SkytabbiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resteurant/bi/poc/skytabbi/SkytabbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resteurant.bi.poc.skytabbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkytabbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
