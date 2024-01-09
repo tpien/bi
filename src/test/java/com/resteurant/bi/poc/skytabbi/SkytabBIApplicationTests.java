4b9d44cab020346be40fd9c9542c9bbad528b74c
TS <kacpper@gmail.com>
2024-01-09 22:31:15 +0100

    Integration test: PlaceRevenue

--- src/test/java/com/resteurant/bi/poc/skytabbi/SkytabBIApplicationTests.java
+++ src/test/java/com/resteurant/bi/poc/skytabbi/SkytabBIApplicationTests.java
@@ -2,6 +2,7 @@ package com.resteurant.bi.poc.skytabbi;
 
 import com.resteurant.bi.poc.skytabbi.loader.ItemGenerator;
 import com.resteurant.bi.poc.skytabbi.model.Item.Item;
+import com.resteurant.bi.poc.skytabbi.model.revenue.PlaceRevenue;
 import com.resteurant.bi.poc.skytabbi.service.ItemService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,95 +27,149 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SkytabBIApplicationTests {
-	@Autowired
-	private ItemService itemService;
+    @Autowired
+    private ItemService itemService;
 
-	@Autowired
-	private ItemGenerator itemGenerator;
+    @Autowired
+    private ItemGenerator itemGenerator;
 
-	@Autowired
-	private ElasticsearchTemplate template;
+    @Autowired
+    private ElasticsearchTemplate template;
 
-	@Container
-	private static final ElasticsearchContainer elasticsearchContainer = new TestsElasticsearchContainer();
+    @Container
+    private static final ElasticsearchContainer elasticsearchContainer = new TestsElasticsearchContainer();
 
-	@BeforeAll
-	static void setUp() {
-		elasticsearchContainer.start();
-	}
+    @BeforeAll
+    static void setUp() {
+        elasticsearchContainer.start();
+    }
 
-	@BeforeEach
-	void testIsContainerRunning() {
-		assertTrue(elasticsearchContainer.isRunning());
-		reloadIndexesLoads();
-	}
+    @BeforeEach
+    void testIsContainerRunning() {
+        assertTrue(elasticsearchContainer.isRunning());
+        reloadIndexesLoads();
+    }
 
-	@Test
-	void testItemsGenerator() {
-		LocalDateTime startDate = LocalDateTime.of(2023, 12, 30, 10, 0, 0);
-		LocalDateTime endDate = LocalDateTime.of(2024, 1, 30, 11, 0, 0);
-		int itemsCount = 10;
-		itemGenerator.generateData(5,itemsCount, startDate,endDate);
+    @Test
+    void testPlaceRevenue() {
+        itemService.create(Item.builder()
+                .place("California")
+                .amount(new BigDecimal(100))
+                .amountGross(new BigDecimal(100))
+                .tax(new BigDecimal(20))
+                .employee("Robert Smith")
+                .saleDate(ItemGenerator.fromStringToDate("2023-12-30T11:55:14"))
+                .build());
 
-		List<Item> items = itemService.getAll();
-		assertNotNull(items);
-		assertEquals(itemsCount, items.size());
+        itemService.create(Item.builder()
+                .place("San Diego")
+                .amount(new BigDecimal(150))
+                .amountGross(new BigDecimal(170))
+                .tax(new BigDecimal(20))
+                .employee("Andrew Ford")
+                .saleDate(ItemGenerator.fromStringToDate("2023-11-28T16:55:14"))
+                .build());
 
-	}
-
-	@Test
-	void testCreateItem() {
-		final Item item = Item.builder()
-				.place("California")
-				.amount(new BigDecimal(100))
-				.amountGross(new BigDecimal(100))
-				.tax(new BigDecimal(20))
-				.employee("Robert Smith")
-				.saleDate(new Date())
-				.build();
+        itemService.create(Item.builder()
+                .place("San Diego")
+                .amount(new BigDecimal(100))
+                .amountGross(new BigDecimal(120))
+                .tax(new BigDecimal(20))
+                .employee("Andrew Ford")
+                .saleDate(ItemGenerator.fromStringToDate("2023-11-30T16:55:14"))
+                .build());
+        PlaceRevenue source1 = PlaceRevenue.builder().name("San Diego").totalAmount(250).totalAmountGross(290).build();
+        PlaceRevenue source2 = PlaceRevenue.builder().name("California").totalAmount(100).totalAmountGross(100).build();
 
 
-		Item createdItem = itemService.create(item);
-		assertNotNull(createdItem);
-		assertNotNull(createdItem.getId());
-		assertEquals(item.getPlace(), createdItem.getPlace());
-		assertEquals(item.getAmount(), createdItem.getAmount());
-		assertEquals(item.getAmountGross(), createdItem.getAmountGross());
-		assertEquals(item.getTax(), createdItem.getTax());
-		assertEquals(item.getEmployee(), createdItem.getEmployee());
-		assertEquals(item.getSaleDate(), createdItem.getSaleDate());
-	}
 
-	@Test
-	void testGetAllItems(){
-		itemService.create(Item.builder()
-				.place("California")
-				.amount(new BigDecimal(100))
-				.amountGross(new BigDecimal(100))
-				.tax(new BigDecimal(20))
-				.employee("Robert Smith")
-				.saleDate(ItemGenerator.fromStringToDate("2023-12-30T11:55:14"))
-				.build());
 
-		itemService.create(Item.builder()
-				.place("San Diego")
-				.amount(new BigDecimal(150))
-				.amountGross(new BigDecimal(170))
-				.tax(new BigDecimal(20))
-				.employee("Andrew Ford")
-				.saleDate(ItemGenerator.fromStringToDate("2023-11-30T16:55:14"))
-				.build());
+        List<PlaceRevenue> result = itemService.getRevenuePlaces(
+                LocalDateTime.parse("2023-11-20T11:55:14"),
+                LocalDateTime.parse("2024-01-30T11:55:14")
+        );
 
-		List<Item> items = itemService.getAll();
-		assertNotNull(items);
-		assertEquals(2, items.size());
-	}
+        assertNotNull(result);
+        assertEquals(result.size(), 2);
+        assertEquals(source1.getName(), result.get(0).getName());
+        assertEquals(source1.getTotalAmount(), result.get(0).getTotalAmount());
+        assertEquals(source1.getTotalAmountGross(), result.get(0).getTotalAmountGross());
 
-	@Test
-	void reloadIndexesLoads() {
-		if (template.indexOps(Item.class).exists()) {
-			template.indexOps(Item.class).delete();
-			template.indexOps(Item.class).create();
-		}
-	}
+        System.out.println(result.get(1));
+        assertEquals(source2.getName(), result.get(1).getName());
+        assertEquals(source2.getTotalAmount(), result.get(1).getTotalAmount());
+        assertEquals(source2.getTotalAmountGross(), result.get(1).getTotalAmountGross());
+
+
+
+    }
+
+    @Test
+    void testItemsGenerator() {
+        LocalDateTime startDate = LocalDateTime.of(2023, 12, 30, 10, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(2024, 1, 30, 11, 0, 0);
+        int itemsCount = 10;
+        itemGenerator.generateData(5, itemsCount, startDate, endDate);
+
+        List<Item> items = itemService.getAll();
+        assertNotNull(items);
+        assertEquals(itemsCount, items.size());
+
+    }
+
+    @Test
+    void testCreateItem() {
+        final Item item = Item.builder()
+                .place("California")
+                .amount(new BigDecimal(100))
+                .amountGross(new BigDecimal(100))
+                .tax(new BigDecimal(20))
+                .employee("Robert Smith")
+                .saleDate(new Date())
+                .build();
+
+
+        Item createdItem = itemService.create(item);
+        assertNotNull(createdItem);
+        assertNotNull(createdItem.getId());
+        assertEquals(item.getPlace(), createdItem.getPlace());
+        assertEquals(item.getAmount(), createdItem.getAmount());
+        assertEquals(item.getAmountGross(), createdItem.getAmountGross());
+        assertEquals(item.getTax(), createdItem.getTax());
+        assertEquals(item.getEmployee(), createdItem.getEmployee());
+        assertEquals(item.getSaleDate(), createdItem.getSaleDate());
+    }
+
+    @Test
+    void testGetAllItems() {
+        itemService.create(Item.builder()
+                .place("California")
+                .amount(new BigDecimal(100))
+                .amountGross(new BigDecimal(100))
+                .tax(new BigDecimal(20))
+                .employee("Robert Smith")
+                .saleDate(ItemGenerator.fromStringToDate("2023-12-30T11:55:14"))
+                .build());
+
+        itemService.create(Item.builder()
+                .place("San Diego")
+                .amount(new BigDecimal(150))
+                .amountGross(new BigDecimal(170))
+                .tax(new BigDecimal(20))
+                .employee("Andrew Ford")
+                .saleDate(ItemGenerator.fromStringToDate("2023-11-30T16:55:14"))
+                .build());
+
+        List<Item> items = itemService.getAll();
+        assertNotNull(items);
+        assertEquals(2, items.size());
+    }
+
+    @Test
+    void reloadIndexesLoads() {
+        if (template.indexOps(Item.class).exists()) {
+            template.indexOps(Item.class).delete();
+            template.indexOps(Item.class).create();
+        }
+    }
 }
